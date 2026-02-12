0939dc8d54c8000a50fe5b370fb82cc8965bd2db
maxsimys555 <maxsimys2001@gmail.com>
2026-02-12 16:06:28 +0200

    fix image

--- src/components/ImagePair/ImagePair.tsx
+++ src/components/ImagePair/ImagePair.tsx
@@ -6,9 +6,10 @@ type ImagePairProps = {
   className?: string;
   leftAlt?: string;
   rightAlt?: string;
-  width?: number;
-  height?: number;
 };
+
+const IMAGE_SIZES = "(min-width: 1024px) 150px, (min-width: 640px) 128px, 96px";
+const IMAGE_CLASS_NAME = "w-24 h-24 sm:w-32 sm:h-32 lg:w-[150px] lg:h-[150px] object-contain";
 
 export default function ImagePair({
   leftSrc,
@@ -16,13 +17,29 @@ export default function ImagePair({
   className = "",
   leftAlt = "emoji",
   rightAlt = "emoji",
-  width = 150,
-  height = 150,
 }: ImagePairProps) {
   return (
     <div className={["flex justify-between", className].filter(Boolean).join(" ")}>
-      <Image src={leftSrc} alt={leftAlt} width={width} height={height} />
-      <Image src={rightSrc} alt={rightAlt} width={width} height={height} />
+      <Image
+        src={leftSrc}
+        alt={leftAlt}
+        width={150}
+        height={150}
+        className={IMAGE_CLASS_NAME}
+        loading="lazy"
+        sizes={IMAGE_SIZES}
+        quality={70}
+      />
+      <Image
+        src={rightSrc}
+        alt={rightAlt}
+        width={150}
+        height={150}
+        className={IMAGE_CLASS_NAME}
+        loading="lazy"
+        sizes={IMAGE_SIZES}
+        quality={70}
+      />
     </div>
   );
 }
